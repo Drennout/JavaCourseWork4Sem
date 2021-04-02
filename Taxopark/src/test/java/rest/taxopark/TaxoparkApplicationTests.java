c823be9ec01f74f48379f6bb3aad6674bb1a79e8
drennout <serega10lad@gmail.com>
2021-04-02 18:06:08 +0300

    Project creation

---
+++ Taxopark/src/test/java/rest/taxopark/TaxoparkApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.taxopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxoparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
